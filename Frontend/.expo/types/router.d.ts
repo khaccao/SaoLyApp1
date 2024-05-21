80b34b008260658629b4bdcbc4b8622327ba4f80
caonkgch211143 <165747202+khaccao@users.noreply.github.com>
2024-05-21 11:48:58 +0700

    .

--- Frontend/.expo/types/router.d.ts
+++ Frontend/.expo/types/router.d.ts
@@ -3,7 +3,7 @@ import type { ReactNode } from 'react';
 import type { TextProps, GestureResponderEvent } from 'react-native';
 
 export namespace ExpoRouter {
-  type StaticRoutes = `/` | `/(tabs)` | `/_sitemap` | `/explore` | `/khoahoc` | `/khoahocdangki` | `/login` | `/login2`;
+  type StaticRoutes = `/` | `/(tabs)` | `/(tabs)\khoahoc` | `/(tabs)\khoahocdangki` | `/_sitemap` | `/explore` | `/khoahoc` | `/login` | `/login2`;
   type DynamicRoutes<T extends string> = never;
   type DynamicRouteTemplate = never;
 
